54a5eadf77946e3bf2e2ba2bbaa7939c5da9559d
Gustavo <11652547+gofeuer@users.noreply.github.com>
2024-07-30 23:13:58 -0300

    Added tests for Proxy.ServeHTTP

--- middleware_test.go
+++ middleware_test.go
@@ -1,9 +1,106 @@
 package l402
 
 import (
+	"errors"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	macaroon "gopkg.in/macaroon.v2"
 )
+
+func TestProxy_ServeHTTP(t *testing.T) {
+	tests := map[string]struct {
+		authorizationHeader    string
+		approveAccess          func(*http.Request, macaroon.Macaroon, Identifier) Rejection
+		expectedAuthenticator  spyHandler
+		apiHandler             spyHandler
+		expectedError          spyHandler
+		expectedResponseStatus int
+	}{
+		"no authorization": {
+			expectedAuthenticator: spyHandler{
+				called:          true,
+				cancelCause:     ErrPaymentRequired,
+				replyStatusCode: http.StatusPaymentRequired,
+			},
+			expectedResponseStatus: http.StatusPaymentRequired,
+		},
+		"defective macaroon": {
+			authorizationHeader: "L402 AGIAJEemVQUTEyNCR0exk7ek90Cg==:79852a0791225dee00be0a6cf31a1619782c21d35995e118bfc74ad812174035",
+			expectedError: spyHandler{
+				called:          true,
+				cancelCause:     ErrInvalidMacaroon,
+				replyStatusCode: http.StatusBadRequest,
+			},
+			expectedResponseStatus: http.StatusBadRequest,
+		},
+		"invalid preimage": {
+			authorizationHeader: "L402 AgJCAABmaHqt+GK9d2yPwYuOn44gCJcUhW7iM7OQKlkdDV8pJQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGIPYUpoJjGXj6TR3qNyibnh+n2R1Dj5HEt5dV4GfbU0jX:0000000000000000000000000000000000000000000000000000000000000001",
+			expectedError: spyHandler{
+				called:          true,
+				cancelCause:     ErrInvalidPreimage,
+				replyStatusCode: http.StatusBadRequest,
+			},
+			expectedResponseStatus: http.StatusBadRequest,
+		},
+		"rejected access": {
+			authorizationHeader: "L402 AgJCAABmaHqt+GK9d2yPwYuOn44gCJcUhW7iM7OQKlkdDV8pJQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGIPYUpoJjGXj6TR3qNyibnh+n2R1Dj5HEt5dV4GfbU0jX:0000000000000000000000000000000000000000000000000000000000000000",
+			approveAccess: func(r *http.Request, m macaroon.Macaroon, i Identifier) Rejection {
+				return ErrPaymentRequired
+			},
+			expectedAuthenticator: spyHandler{
+				called:          true,
+				cancelCause:     ErrPaymentRequired,
+				replyStatusCode: http.StatusPaymentRequired,
+			},
+			expectedResponseStatus: http.StatusPaymentRequired,
+		},
+		"success": {
+			authorizationHeader: "L402 AgJCAABmaHqt+GK9d2yPwYuOn44gCJcUhW7iM7OQKlkdDV8pJQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGIPYUpoJjGXj6TR3qNyibnh+n2R1Dj5HEt5dV4GfbU0jX:0000000000000000000000000000000000000000000000000000000000000000",
+			approveAccess: func(r *http.Request, m macaroon.Macaroon, i Identifier) Rejection {
+				return nil // access approved
+			},
+			apiHandler: spyHandler{
+				called: true,
+			},
+			expectedResponseStatus: http.StatusOK,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			authenticator := spyHandler{replyStatusCode: test.expectedAuthenticator.replyStatusCode}
+			accessAuthority := mockAccessAuthority{test.approveAccess}
+			apiHandler := spyHandler{replyStatusCode: test.apiHandler.replyStatusCode}
+			errorHandler := spyHandler{replyStatusCode: test.expectedError.replyStatusCode}
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", "/some_proctected_resource", nil)
+			r.Header.Set("Authorization", test.authorizationHeader)
+
+			Proxy(nil, accessAuthority, WithAuthenticator(&authenticator), WithErrorHandler(&errorHandler))(&apiHandler).ServeHTTP(w, r)
+
+			response := w.Result()
+
+			if authenticator.called != test.expectedAuthenticator.called {
+				t.Errorf("expected: %v but got: %v", test.expectedAuthenticator.called, authenticator.called)
+			}
+
+			if errorHandler.called != test.expectedError.called {
+				t.Errorf("expected: %v but got: %v", test.expectedError.called, errorHandler.called)
+			}
+
+			if !(errors.Is(errorHandler.cancelCause, test.expectedError.cancelCause) ||
+				errorHandler.cancelCause.Error() == test.expectedError.cancelCause.Error()) {
+				t.Errorf("expected: %s but got: %s", test.expectedError.cancelCause.Error(), errorHandler.cancelCause.Error())
+			}
+
+			if response.StatusCode != test.expectedResponseStatus {
+				t.Errorf("expected: %d but got: %d", test.expectedResponseStatus, response.StatusCode)
+			}
+		})
+	}
+}
 
 func TestGetL402AuthorizationHeader(t *testing.T) {
 	tests := map[string]struct {
@@ -79,4 +176,12 @@ func TestValidatePreimage(t *testing.T) {
 			}
 		})
 	}
+}
+
+type mockAccessAuthority struct {
+	approveAccess func(*http.Request, macaroon.Macaroon, Identifier) Rejection
+}
+
+func (a mockAccessAuthority) ApproveAccess(r *http.Request, m macaroon.Macaroon, i Identifier) Rejection {
+	return a.approveAccess(r, m, i)
 }
